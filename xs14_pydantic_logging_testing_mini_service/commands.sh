3668cf40b47044a9d9912ddde042d984f7a82c97
Milos Faktor <faktor.milos@gmail.com>
2026-02-01 09:02:04 +0100

    xs14_pydantic_logging_testing_mini_service/commands.sh

--- xs14_pydantic_logging_testing_mini_service/commands.sh
+++ xs14_pydantic_logging_testing_mini_service/commands.sh
@@ -6,14 +6,12 @@ source .venv/bin/activate
 deactivate
 pip freeze > requirements.txt
 
-# python module run
-PYTHONPATH=src python -m xs14.main
-PYTHONPATH=src python sandbox.py
 
 # set PYTHONPATH into .venv
 source .venv/bin/activate && export PYTHONPATH="$(pwd)/src" && echo $PYTHONPATH
 
+
 # and after i can use only
 python -m xs14.main
-python sandbox.py
+
 
